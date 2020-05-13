cff6764007d102ff69f2888031a57b0f31afdd18
Swaraj <swarajdalmia.bitspilani@gmail.com>
2020-05-14 00:09:11 +0530

    Readme on RNNs and LSTMs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Machine Learning Playground 
+Contains the implementation of some popular ML/DL models and architectures.
+
+The code used to implement the Recurrent Neuural Networks(RNN) model using Long-Short Term Memory(LSTM) units with a was taken from [here](https://github.com/WillKoehrsen/recurrent-neural-networks/blob/master/notebooks/Deep%20Dive%20into%20Recurrent%20Neural%20Networks.ipynb). 
+
+
+
+
---
+++ src/RNN.py
@@ -0,0 +1,15 @@
+# https://towardsdatascience.com/recurrent-neural-networks-by-example-in-python-ffd204f99470
+# https://github.com/WillKoehrsen/recurrent-neural-networks/blob/master/notebooks/Deep%20Dive%20into%20Recurrent%20Neural%20Networks.ipynb
+
+import warnings
+from tensorflow.python.client import device_lib
+import pandas as pd
+import numpy as np
+
+def func():
+    print(device_lib.list_local_devices())
+
+
+if __name__ == "__main__":
+    func()
+
